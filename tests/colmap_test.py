6379331c080a1f6f2fe700f77443396ef25a80d6
stash <pomichterstash@gmail.com>
2024-12-17 14:22:23 +0900

    moved /demos to /tests

---
+++ tests/colmap_test.py
@@ -0,0 +1,11 @@
+import sys
+import os
+
+# Add the parent directory of 'demos' to the Python path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+# Now try to import
+from dimos.environment.colmap_environment import COLMAPEnvironment
+
+env = COLMAPEnvironment()
+env.initialize_from_video("data/IMG_1525.MOV", "data/frames")
