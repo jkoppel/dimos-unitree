1565ca5383e608b130bb92004dce90e06bb26f23
lesh <lesh@sysphere.org>
2025-04-16 14:25:47 -0700

    dev.sh now does building as well

--- builddev.sh
+++
@@ -1,5 +0,0 @@
-docker rm -f dimos-dev
-docker build \
-    --build-arg GIT_COMMIT=$(git rev-parse --short HEAD) \
-    --build-arg GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD) \
-    -t dimensionalos/dev-base docker/dev/base/
--- dev.sh
+++ dev.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+
+if [ "$1" == "build" ]; then
+    docker rm -f dimos-dev
+    docker build \
+        --build-arg GIT_COMMIT=$(git rev-parse --short HEAD) \
+        --build-arg GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD) \
+        -t dimensionalos/dev-base docker/dev/base/
+fi
+
 docker-compose -f docker/dev/base/docker-compose.yaml up -d && docker exec -it dimos-dev bash
